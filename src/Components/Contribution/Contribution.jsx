5caf74e9bfbcc396adb804d34502635cb7f8d2be
smita.driveitdigital <smita.driveitdigital@gmail.com>
2026-02-17 17:42:02 +0530

    add contribution page

--- src/Components/Contribution/Contribution.jsx
+++ src/Components/Contribution/Contribution.jsx
@@ -1,7 +1,66 @@
+import { Table } from "react-bootstrap";
+
 export default function Contribution() {
-
-
+    const articlesData = [
+        {
+    id: 1,
+    title: "Heads up! Because of the more complicated",
+    date: "Friday, 2 January 2026",
+    status: "Publish",
+    image: "https://www.theasianbanker.com/uploads/tabmanila/img_6937e9f94f2d15.19246745.jpg"
+  },
+  {
+    id: 2,
+    title: "Heads up! Because of the more complicated",
+    date: "Friday, 2 January 2026",
+    status: "Pending",
+    image: "https://www.theasianbanker.com/uploads/tabmanila/img_6937e9f94f2d15.19246745.jpg"
+  },
+  {
+    id: 3,
+    title: "Heads up! Because of the more complicated",
+    date: "Friday, 2 January 2026",
+    status: "Reject",
+    image: "https://www.theasianbanker.com/uploads/tabmanila/img_6937e9f94f2d15.19246745.jpg"
+  },
+  {
+    id: 4,
+    title: "Heads up! Because of the more complicated",
+    date: "Friday, 2 January 2026",
+    status: "Reject",
+    image: "https://www.theasianbanker.com/uploads/tabmanila/img_6937e9f94f2d15.19246745.jpg"
+  }
+    ]
     return(
-        <></>
+       <main>
+        <div className="table-wrapper">
+            <Table>
+               <thead>  
+                <tr>
+                    <th>Articles image</th>
+                    <th>Articles title</th>
+                    <th>Date</th>
+                    <th>Status</th>
+                </tr>
+               </thead>
+               <tbody>
+                {
+                    articlesData.map((data, index) => {
+                        return(
+                            <tr key={index}>
+                    <td>{data.id}</td>
+                    <td>{data.title}</td>
+                    <td>{data.date}</td>
+                    <td>{data.status}</td>
+                    <td><img src={data.image} alt={data.title}/></td>
+                </tr>
+                        )
+                    })
+                }
+                
+               </tbody>
+            </Table>
+        </div>
+       </main>
     );
 }
--- src/Components/MyAccount/MyAccount.jsx
+++ src/Components/MyAccount/MyAccount.jsx
@@ -1,11 +1,39 @@
 import {Card, Row, Col} from 'react-bootstrap';
 import { CardBody, CardTitle } from 'reactstrap';
+import { MessageSquare , Mail, CircleFadingPlus, Bookmark } from 'lucide-react';
+import './MyAccount.css';
 export default function MyAccount() {
+    const myAccountlist = [
+        {
+            id: "my-conversations",
+            icon: MessageSquare,
+            label: "My Conversations",
+            href: "/",
+        },
+        {
+            id: "bookmarks",
+            icon: Bookmark,
+            label: "Bookmarks",
+            href: "/",
+        },
+        {
+            id: "newsletters",
+            icon: Mail,
+            label: "Newsletters",
+            href: "/",
+        },
+        {
+            id: "get-connected",
+            icon: CircleFadingPlus ,
+            label: "Get Connected",
+            href: "/",
+        },
+    ]
     return(
         <main>
             <div>
                 <Row>
-                    <Col className='' lg="12" sm="12" md="12">
+                    <Col className='mb-4' lg="12" sm="12" md="12">
                         <Card>
                             <CardBody>
                                 <div className='d-flex gap-2 align-items-center'>
@@ -18,6 +46,30 @@ export default function MyAccount() {
                             </CardBody>
                         </Card>
                     </Col>
+                    <Col className='' lg="12" sm="12" md="12">
+                        <Row>
+                            {myAccountlist.map((item, index) => {
+                        return(
+                            <Col key={index} className='' lg="3" sm="12" md="6">
+                        <Card>
+                            <CardBody>
+                                <div className='infoCard'>
+                                    <div className='p-3 icon-box border rounded-circle mb-4'>
+                                        <item.icon className='' size={25} />
+                                    </div>
+                                    <div>
+                                        <p className='infoTitle'>{item.label}</p>
+                                    </div>
+                                </div>
+                            </CardBody>
+                        </Card>
+                    </Col>
+                        )
+                    })}
+                        </Row>
+                    </Col>
+                    
+                   
                 </Row>
             </div>
         </main>
